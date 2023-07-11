e7188d417b1bf07bd7cecc0b6c90ffd1c8eeeeed
Patricia Naomi <patricianaomiyama@gmail.com>
2023-07-11 19:22:28 -0300

    finalização exercício

---
+++ 05-07-23 JS ex1/index.js
@@ -0,0 +1,14 @@
+function funcao01() {
+    // aqui vamos pegar a id que iremos fazer a alteração
+    document.getElementById("altera-texto").innerHTML="Texto alterado <b>01</b>";
+
+    //lembrar de criar uma variável
+    var obj01 = document.getElementById("id-body");
+    obj01.className="alt01";
+}
+
+function funcao02() {
+    document.getElementById("altera-texto").innerHTML="Texto alterado <b>02</b>";
+    var obj01 = document.getElementById("id-body");
+    obj01.className="alt02";
+}
