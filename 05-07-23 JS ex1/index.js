369afb80d486a4f3576e89c49cdd0ea899fcb488
Patricia Naomi <patricianaomiyama@gmail.com>
2023-07-11 19:55:38 -0300

    alterações root

--- 05-07-23 JS ex1/index.js
+++ 05-07-23 JS ex1/index.js
@@ -1,6 +1,6 @@
 function funcao01() {
     // aqui vamos pegar a id que iremos fazer a alteração
-    document.getElementById("altera-texto").innerHTML="Texto alterado <b>01</b>";
+    document.getElementById("altera-texto").innerHTML="Texto alterado<b>01</b>";
 
     //lembrar de criar uma variável
     var obj01 = document.getElementById("id-body");
@@ -8,7 +8,7 @@ function funcao01() {
 }
 
 function funcao02() {
-    document.getElementById("altera-texto").innerHTML="Texto alterado <b>02</b>";
+    document.getElementById("altera-texto").innerHTML="Texto alterado<b>02</b>";
     var obj01 = document.getElementById("id-body");
     obj01.className="alt02";
 }
