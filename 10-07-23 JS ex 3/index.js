cbb293a29fda3a18b950224ea3b6e55fc43dc5cc
Patricia Naomi <patricianaomiyama@gmail.com>
2023-07-12 19:25:10 -0300

    finalização exercício 3

---
+++ 10-07-23 JS ex 3/index.js
@@ -0,0 +1,13 @@
+function botao() {
+    var resp = document.getElementById('campo1').value;
+
+    if(resp == "sim") {
+        document.getElementById("recebe").innerHTML = "&#128525;";
+    } else if (resp == "não") {
+        
+        document.getElementById("recebe").innerHTML = "&#128531;";
+    } else {
+        document.getElementById("recebe").innerHTML="Digite uma opção válida.";
+        
+    }
+}
