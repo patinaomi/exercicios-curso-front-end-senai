a7ac5ff26a15f14061fcda63535b3f98d4704d09
Patricia Naomi <patricianaomiyama@gmail.com>
2023-07-11 21:55:34 -0300

    finalização exercício 2

---
+++ 05-07-23 JS ex 2/index.js
@@ -0,0 +1,71 @@
+function vogais() {
+    document.getElementById("quadro1").innerHTML="A";
+    var obj1 = document.getElementById("quadro1");
+    obj1.className = "azul";
+
+    document.getElementById("quadro2").innerHTML="B";
+    var obj2 = document.getElementById("quadro2");
+    obj2.className = "vermelho";
+
+    document.getElementById("quadro3").innerHTML="C";
+    var obj3 = document.getElementById("quadro3");
+    obj3.className = "verde";
+
+    document.getElementById("quadro4").innerHTML="D";
+    var obj4 = document.getElementById("quadro4");
+    obj4.className = "amarelo";
+}
+
+function romanos() {
+    document.getElementById("quadro1").innerHTML="I";
+    var obj1 = document.getElementById("quadro1");
+    obj1.className = "vermelho";
+
+    document.getElementById("quadro2").innerHTML="II";
+    var obj2 = document.getElementById("quadro2");
+    obj2.className = "verde";
+
+    document.getElementById("quadro3").innerHTML="III";
+    var obj3 = document.getElementById("quadro3");
+    obj3.className = "amarelo";
+
+    document.getElementById("quadro4").innerHTML="IV";
+    var obj4 = document.getElementById("quadro4");
+    obj4.className = "azul";
+}
+
+function vogaisGrid() {
+  document.getElementById("grid1").innerHTML = "A";
+  var obj1 = document.getElementById("grid1");
+  obj1.className = "azul";
+
+  document.getElementById("grid2").innerHTML = "B";
+  var obj2 = document.getElementById("grid2");
+  obj2.className = "vermelho";
+
+  document.getElementById("grid3").innerHTML = "C";
+  var obj3 = document.getElementById("grid3");
+  obj3.className = "verde";
+
+  document.getElementById("grid4").innerHTML = "D";
+  var obj4 = document.getElementById("grid4");
+  obj4.className = "amarelo";
+}
+
+function romanosGrid() {
+  document.getElementById("grid1").innerHTML = "I";
+  var obj1 = document.getElementById("grid1");
+  obj1.className = "vermelho";
+
+  document.getElementById("grid2").innerHTML = "II";
+  var obj2 = document.getElementById("grid2");
+  obj2.className = "verde";
+
+  document.getElementById("grid3").innerHTML = "III";
+  var obj3 = document.getElementById("grid3");
+  obj3.className = "amarelo";
+
+  document.getElementById("grid4").innerHTML = "IV";
+  var obj4 = document.getElementById("grid4");
+  obj4.className = "azul";
+}
