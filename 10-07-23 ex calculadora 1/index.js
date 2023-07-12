b63c8b522442a8464cb308fca210cecbcc0c9aeb
Patricia Naomi <patricianaomiyama@gmail.com>
2023-07-12 20:36:45 -0300

    finalização exercício calculadora 1

--- 10-07-23 ex calculadora 1/index.js
+++ 10-07-23 ex calculadora 1/index.js
@@ -1,7 +1,36 @@
-var resultado = 0;
 
 function somar() {
     var num1 = parseInt(document.getElementById("campo1").value);
     var num2 = parseInt(document.getElementById("campo2").value);
-    document.getElementById()
+
+    var resultado = num1 + num2;
+    
+    document.getElementById("recebe").innerHTML = resultado;
+}
+
+function subtrair() {
+    var num1 = parseInt(document.getElementById("campo1").value);
+    var num2 = parseInt(document.getElementById("campo2").value);
+
+    var resultado = num1 - num2;
+
+    document.getElementById("recebe").innerHTML = resultado;
+}
+
+function multiplicar() {
+    var num1 = parseInt(document.getElementById("campo1").value);
+    var num2 = parseInt(document.getElementById("campo2").value);
+
+    var resultado = num1 * num2;
+
+    document.getElementById("recebe").innerHTML = resultado;
+}
+
+function dividir() {
+    var num1 = parseInt(document.getElementById("campo1").value);
+    var num2 = parseInt(document.getElementById("campo2").value);
+
+    var resultado = num1 / num2;
+
+    document.getElementById("recebe").innerHTML = resultado;
 }
