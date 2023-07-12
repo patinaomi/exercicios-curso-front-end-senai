48b0988606382b136ff942445b5d92721a245c4e
Patricia Naomi <patricianaomiyama@gmail.com>
2023-07-12 19:41:20 -0300

    alterações utf-8

---
+++ 10-07-23 ex calculadora 1/index.js
@@ -0,0 +1,7 @@
+var resultado = 0;
+
+function somar() {
+    var num1 = parseInt(document.getElementById("campo1").value);
+    var num2 = parseInt(document.getElementById("campo2").value);
+    document.getElementById()
+}
