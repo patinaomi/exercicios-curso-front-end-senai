ea51e2b2167d030d89fad9bdfa354483d3a5910c
Patricia Naomi <patricianaomiyama@gmail.com>
2023-07-17 22:23:22 -0300

    exercício finalizado

---
+++ 17-07-23/index.js
@@ -0,0 +1,14 @@
+function somar() {
+  var t1 = parseInt(document.getElementById("campo1").value);
+  var t2 = parseInt(document.getElementById("campo2").value);
+
+  var soma = t1 + t2;
+
+  document.getElementById("resultado-p").innerHTML = soma;
+  document.getElementById("resultado-tabela").innerHTML = soma;
+  document.getElementById("resultado-div").innerHTML = soma;
+  var obj3 = document.getElementById("resultado-div");
+  obj3.className = "container";
+
+  var resultado = (document.getElementById("resultado-campo").value = soma);
+}
