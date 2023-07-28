6cbf6ed97617b5683e10d356fe8137df7dda05e6
Patricia Naomi <patricianaomiyama@gmail.com>
2023-07-27 21:16:57 -0300

    refatorando código

--- 27-07-23/index.js
+++ 27-07-23/index.js
@@ -1,44 +1,94 @@
-//ex com erro
+function result() {
+  var resultCartela = "";
+  var resultSorteio = "";
+  var resultAcertos = "";
+  var cont;
+  var megasena = 1;
+  var acertos = 0;
 
-function bubbleSort(array) {
-  for (let i = 0; i < array.length; i++) {
-    for (let j = i + 1; j < array.length; j++) {
-      if (array[i] > array[j]) {
-        let aux = array[i];
-        array[i] = array[j];
-        array[j] = aux;
+  vetorCartela = new Array();
+
+  var lin, col, t;
+
+  vetorResultados = new Array();
+
+  while (cont != 0) {
+    cont++;
+    cont = 0;
+
+    //criação vetor unidimensional - cartela
+    for (lin = 0; lin < 6; lin++) {
+      vetorCartela[lin] = Math.floor(Math.random() * 60 + 1);
+    }
+
+    //criação vetor bidimensional - resultados
+    for (lin = 0; lin < 10; lin++) {
+      vetorResultados[lin] = new Array();
+
+      for (col = 0; col < 6; col++) {
+        vetorResultados[lin][col] = Math.floor(Math.random() * 60 + 1);
+      }
+    }
+
+    //elimina números repetidos do vetor - cartela
+    for (lin = 0; lin < 6; lin++) {
+      for (j = 1 + lin; j < 6; j++) {
+        if (vetorCartela[j] == vetorCartela[lin]) {
+          cont++;
+        }
+      }
+    }
+
+    //elimina números repetidos do vetor - resultado
+    for (lin = 0; lin < 10; lin++) {
+      for (col = 0; col < 6; col++) {
+        for (l = 1 + col; l < 6; l++) {
+          if (vetorResultados[lin][col] == vetorResultados[lin][l]) {
+            cont++;
+          }
+        }
       }
     }
   }
-}
-
-var pontos = 0;
-function impr() {
-  var result = "";
-  var semordernar = "";
-  vetor1 = [5, 3, 10, 1, 24, 8];
-  vetor2 = [3, 24, 8, 2, 45, 36];
-
-
-
+  // apresentação vetor cartela
   for (i = 0; i < 6; i++) {
-    bubbleSort(vetor1);
-    semordernar += "[" + vetor1[i] + "] ";
+    resultCartela += "[" + vetorCartela[i] + "] ";
   }
 
-  for (i = 0; i < 6; i++) {
-    bubbleSort(vetor2);
-    result += "[" + vetor2[i] + "] ";
-  }
-
-  for (i = 0; i < 6; i++) {
-    if (vetor2[i] == vetor1[i]) {
-      pontos++;
+  // apresentação vetor resultados
+  for (lin = 0; lin < 10; lin++) {
+    resultSorteio += "Resultado Sorteio <br>" + megasena + "<br>";
+    for (col = 0; col < 6; col++) {
+      resultSorteio += "[" + vetorResultados[lin][col] + "] ";
     }
 
-}
+    resultSorteio += "<br><hr>";
+    megasena++;
+  }
 
-document.getElementById("resultado-cont").innerHTML = pontos;
-  document.getElementById("resultado-ordem").innerHTML = semordernar;
-  document.getElementById("resultado").innerHTML = result;
+  // vetorAcertos = new Array();
+
+  //   for(lin=0; lin<10; lin++) {
+  //       for(col = 0; col<6; col++) {
+
+  //         for(t=1+col; t<6; t++) {
+
+  //             if(vetorCartela[lin][col] == vetorResultados[lin][t]) {
+  //                 acertos++;
+  //               }
+  //         }
+  //          }
+  //         }
+
+  //         console.log(acertos);
+
+  const vetorAcertos = vetorCartela.map(myFunction);
+
+  function myFunction(num) {
+    return num * 10;
+  }
+
+  document.getElementById("resultado-cartela").innerHTML = resultCartela;
+  document.getElementById("resultado-sorteio").innerHTML = resultSorteio;
+  document.getElementById("resultado-acertos").innerHTML = resultAcertos;
 }
